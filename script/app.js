fe5578cbbc56f1c83116f60f6a7db205b3e69e41
Zakaria El Hamdaouit <zakaria.el.hamdaoui@student.howest.be>
2023-10-11 10:47:59 +0200

    commit 1

--- script/app.js
+++ script/app.js
@@ -1,5 +1,47 @@
+
+const jokeContent = document.querySelector('.js-joke-content');
+const jokeBtn = document.querySelector('.js-joke-btn');
+const jokeLoader = document.querySelector('.js-loading-state');
+
+let loadingDelay;
+
+const fetchJoke = () => {
+    return fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            Accept: "application/json"
+        }
+    })
+        .then((response) => response.json())
+}
+
+const showloader = () => {
+    loadingDelay = setTimeout(() => {
+        jokeLoader.classList.remove('u-hidden');
+    }, 500);
+    jokeContent.classList.add('u-hidden');
+}
+
+const removeLoeader = () => {
+    if (loadingDelay) {
+        clearTimeout(loadingDelay);
+        loadingDelay = null;
+    }
+    jokeLoader.classList.add('u-hidden');
+    jokeContent.classList.remove('u-hidden');
+}
+
+const generateJoke = async () => {
+    showloader();
+    const { joke } = await fetchJoke();
+    jokeContent.innerHTML = joke;
+    removeLoeader();
+}
+
 const init = function () {
     console.log('script loaded');
+    generateJoke();
+
+    jokeBtn.addEventListener('click', generateJoke);
 }
 
 init();
